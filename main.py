3bb3785b4ed508116e2b346dd068b78522aa24a3
tserekh <kvertea@gmial.com>
2021-03-10 03:43:08 +0300

    select metro

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, render_template, url_for
-
-
+from manage MetroStation
+import pandas as pd
 app = Flask(__name__, static_url_path='/static')
 
 @app.route('/', methods=['POST', 'GET'])
@@ -8,6 +8,17 @@ def show_map():
     return render_template('points.html', have_logos=[])
 
 
+@app.route('/get_metros', methods=['GET'])
+def get_metros(request):
+
+    lat_min = request.GET.get('lat_min')
+    lon_min = request.GET.get('lon_min')
+    lat_max = request.GET.get('lat_max')
+    lon_max = request.GET.get('lon_max')
+    df = pd.read_csv('/home/tserekh/metro.csv')
+
+    filt = (df['lat'] > lat_min) & (df['lat'] < lat_max) & (df['lon'] > lon_min) & (df['lon'] < lon_max)
+    df = df[filt]
+    return {'metros':list(df.T.to_dict().values())}
 if __name__ == "__main__":
-    print(url_for("static", filename="js/homes.js"))
     app.run(port=5000, debug=True)
