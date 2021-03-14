b0f38f0d08f0eb7fee5e8d7efc2ac6cee037ac33
tserekh <kvertea@gmial.com>
2021-03-14 18:48:41 +0300

    all done

--- main.py
+++ main.py
@@ -44,25 +44,25 @@ def create_coord_filter(request):
 @app.route('/get_metros', methods=['GET'])
 def get_metros():
     coord_filter = create_coord_filter(request)
-    df = pd.read_sql(f'select count(*) from postgres.metros where 1=1', con)
+    df = pd.read_sql(f'select count(*) from postgres.metros where 1=1 and {coord_filter}', con)
     count = df.iloc[0, 0]
-    if count > 60:
-        df = pd.read_sql(f'select * from postgres.metro_stations where 1=1 and {coord_filter}', con)
-    else:
+    if count < 60:
         df = pd.read_sql(f'select * from postgres.metros where 1=1 and {coord_filter}', con)
+    else:
+        df = pd.read_sql(f'select * from postgres.metro_stations where 1=1 and {coord_filter}', con)
     result = list(df.T.to_dict().values())
     return {'metros': result}
 
 
 @app.route('/get_ground_stops', methods=['GET'])
-def get_ground_stops(request):
+def get_ground_stops():
     n_clusters = 200
-    agg = {'n_lines': 'size'}
+    agg = {'source_name': 'size'}
     coord_filter = create_coord_filter(request)
     df = pd.read_sql(f'select * from postgres.ground_stops where 1=1 and {coord_filter}', con)
     if len(df):
         df_clusters = get_clusters(df, n_clusters, agg, 'stupid')
-        df_clusters.rename(columns={'n_lines': 'n_ground_stops'}, inplace=True)
+        df_clusters.rename(columns={'source_name': 'n_ground_stops'}, inplace=True)
         context = {
             'ground_stops': list(df_clusters.T.to_dict().values()),
         }
