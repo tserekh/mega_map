28b7b97de13607ab686d454b2b5d47d5db4cb1db
Artem Tserekh <artem.tserekh@x5.ru>
2021-12-07 20:45:05 +0300

    SQLALCHEMY_DATABASE_URI

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
+import os
 prod = True
 if prod:
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:postgres@postgres:5432/postgres"
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    # SQLALCHEMY_DATABASE_URI = "postgresql://postgres:postgres@postgres:5432/postgres"
 else:
     SQLALCHEMY_DATABASE_URI = "postgresql://postgres:postgres@localhost:5433/postgres"
