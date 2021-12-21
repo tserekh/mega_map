ae5a4a5861e792c9ae879f483632244560e6c4cf
Artem Tserekh <kvertea@gmail.com>
2021-12-21 22:19:29 +0300

    Update config.py

--- config.py
+++ config.py
@@ -13,5 +13,5 @@ else:
     SQLALCHEMY_DATABASE_URI = "postgresql://postgres:postgres@localhost:5433/postgres"
 
 ### routing area
-lat_max, lon_max = 55.901923, 37.607278
-lat_min, lon_min = 55.848597, 37.524299
+lat_max, lon_max = 56.0, 38.0
+lat_min, lon_min = 55.0, 37.0
