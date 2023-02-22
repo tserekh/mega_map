3ea3d067a1e1d5f8fb34f9246c68933797738525
Artem Tserekh <artem.tserekh@x5.ru>
2023-02-23 02:03:59 +0300

    root user

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ flask db migrate
 echo 'finish db migrate'
 flask db migrate
 echo 'finish db migrate'
-python data_to_db.py
+python3 data_to_db.py
 echo 'finish python data_to_db.py'
 #gunicorn --bind 0.0.0.0:"$PORT"  --workers=1 application
 gunicorn --bind 0.0.0.0:8000  --workers=1 application
